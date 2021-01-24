a2cc78a8b7c5578cc486ca06442d7f0ee5bea419
qingyelvluo <2256280155@qq.com>
2021-01-24 22:44:01 +0800

    Test file with Chinese docstring.

---
+++ file_fromhome.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+def func():
+    """
+    This is English notation, first line.
+    这是第中文注释，第二行。
+    """
+    print("hello world")
+    
+if __name__ == "__main__":
+    func()
